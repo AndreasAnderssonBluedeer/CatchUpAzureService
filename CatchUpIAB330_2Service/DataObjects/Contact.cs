52bc0801b0ea2712febfdfd05b9702320beaedb3
bambismurfen <bambismurfen@gmail.com>
2016-10-24 15:04:36 +1000

    ??da fuq

---
+++ CatchUpIAB330_2Service/DataObjects/Contact.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Azure.Mobile.Server;
+
+
+namespace CatchUpIAB330_2Service.DataObjects
+{
+    public class Contact : EntityData
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        //Key = Email
+        public string Key { get; set; }
+
+
+
+    }
+}
+
